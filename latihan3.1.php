31e15ff365edadb39ee5c67252b2e9cad8931976
tasyaananda24 <tasyanovita233@gmail.com>
2024-08-26 09:11:23 +0700

    Add files via upload

---
+++ latihan3.1.php
@@ -0,0 +1,39 @@
+<?php
+class Kendaraan {
+    var $jumlahRoda=4;
+    var $warna;
+    var $bahanBakar="Premium";
+    var $harga=100000000;
+    var $merek;
+    var $tahunPembuatan=2004;
+    function statusHarga()
+    {
+        if($this->harga>50000000)
+        {
+            return "Harga Kendaraan Mahal";
+
+        }
+        else
+        {
+            return "Harga Kendaraan Murah";
+        }
+    }
+        function statusSubsidi()
+        {
+            if($this ->tahunPembuatan < 2005 && $this->bahanBakar= "Premium")
+            {
+            return "DAPAT SUBSIDI";
+        }
+        else {
+        return "TIDAK DAPAT SUBSIDI";
+        }
+        
+      
+    }
+    
+}
+    //instansiasi kelas
+    $ObjekKendaraan = new Kendaraan();//pembuatan objek dari kelas
+ echo "jumlahRoda : ".$ObjekKendaraan->$jumlahRoda."<br />"; //proses instansiasi pemanggilan variable
+ echo "Status Harga : ".$ObjekKendaraan->$statusHarga();//proses instansiasi//pemanggilan function dari kelas
+ echo "Status Subsidi :".$ObjekKendaraan1->statusSubsidi();
