ba6ea7b6bf939b5be29e3e8fed949a17f7b48f18
tasyaananda24 <tasyanovita233@gmail.com>
2024-10-28 12:59:11 +0700

    Pertemuan11

---
+++ proses_barang.php
@@ -0,0 +1,25 @@
+<?php
+include("koneksi.php");
+$koneksi = new database();
+
+$action = $_GET['action'];
+
+if ($action == "add") {
+    $koneksi->tambah_data($_POST['nama_barang'],$_POST['stok'], $_POST['harga_beli'], $_POST['harga_jual']);
+    header("location:index.php");
+}
+else if ($action == "edit"){
+    $id_barang = $_GET['id_barang'];
+    $koneksi->edit_data($id_barang,  nama_barang:$_POST['nama_barang'],stok : $_POST['stok'], harga_beli: $_POST['harga_beli'], harga_jual: $_POST['harga_jual']);
+    header("location:index.php");
+}   
+else if ($action == "delete"){
+    $id_barang =  $_GET['id_barang'];
+    $koneksi->delete_data(id_barang:$id_barang);
+    header("location:index.php");
+}
+else if ($action == "search"){
+    $nama_barang = $_POST['nama_barang'];
+    $koneksi->cari_data(nama_barang:$nama_barang);
+    header("location:index.php");
+}
